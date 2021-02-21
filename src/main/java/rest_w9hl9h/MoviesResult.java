d4f9efa27ba15332d26a6767c5f6167b352230c5
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-02-21 16:13:22 +0100

    Create MoviesResult wrapper

---
+++ src/main/java/rest_w9hl9h/MoviesResult.java
@@ -0,0 +1,23 @@
+package rest_w9hl9h;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "movies")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MoviesResult {
+    @XmlElement(name = "movie")
+    private List<Movie> movies;
+
+    public List<Movie> getMovies() {
+	return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+	this.movies = movies;
+    }
+}
