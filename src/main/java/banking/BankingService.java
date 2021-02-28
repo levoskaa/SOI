7c60f9e6e98e42e238b6086e77b57b141465856d
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-02-28 22:27:06 +0100

    Initial commit

---
+++ src/main/java/banking/BankingService.java
@@ -0,0 +1,13 @@
+package banking;
+
+import javax.ws.rs.core.Response;
+
+public class BankingService implements IBankingService {
+
+    @Override
+    public Response ChargeCard(String cardNumber, int amount) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
