4aec66d0edd1a73743484b1935b6cf5397e8a4b6
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-03 20:55:34 +0100

    WIP #1

--- src/main/java/banking/BankingService.java
+++ src/main/java/banking/BankingService.java
@@ -1,13 +1,12 @@
 package banking;
 
-import javax.ws.rs.core.Response;
+import banking.Banking.ChargeCardRequest;
+import banking.Banking.ChargeCardResponse;
 
 public class BankingService implements IBankingService {
-
     @Override
-    public Response ChargeCard(String cardNumber, int amount) {
+    public ChargeCardResponse chargeCard(ChargeCardRequest request) {
 	// TODO Auto-generated method stub
 	return null;
     }
-
 }
--- src/main/java/banking/IBankingService.java
+++ src/main/java/banking/IBankingService.java
@@ -4,13 +4,14 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
-@Path("banking")
-@Consumes("application/x-protobuf")
-@Produces("application/x-protobuf")
+import banking.Banking.ChargeCardRequest;
+import banking.Banking.ChargeCardResponse;
+
+@Consumes({ "application/json", "application/x-protobuf" })
+@Produces({ "application/json", "application/x-protobuf" })
 public interface IBankingService {
     @POST
     @Path("ChargeCard")
-    Response ChargeCard(String cardNumber, int amount);
+    ChargeCardResponse chargeCard(ChargeCardRequest request);
 }
