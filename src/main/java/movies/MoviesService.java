7c2553d8fc4c3d2fb8bc617a20761f89da066eb8
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-04 21:50:48 +0100

    Implement MoviesService endpoints

--- src/main/java/movies/MoviesService.java
+++ src/main/java/movies/MoviesService.java
@@ -1,44 +1,103 @@
 package movies;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
 import movies.Movies.Movie;
 import movies.Movies.MovieId;
 import movies.Movies.MovieIdList;
 import movies.Movies.MovieList;
 
 public class MoviesService implements IMoviesService {
+    private static int nextId = 0;
+    private static List<MyMovie> movies = new ArrayList<>();
 
     @Override
     public MovieList getMovies() {
-	// TODO Auto-generated method stub
-	return null;
+	return MovieList.newBuilder().addAllMovie(movies.stream()
+		.map(movie -> movie.getMovie()).collect(Collectors.toList()))
+		.build();
     }
 
     @Override
     public Movie getMovieById(int id) {
-	// TODO Auto-generated method stub
-	return null;
+	Optional<Movie> result = movies.stream()
+		.filter(movie -> movie.getId() == id)
+		.map(movie -> movie.getMovie())
+		.collect(Collectors.reducing((a, b) -> null));
+	if (result.isEmpty()) {
+	    throw new WebApplicationException(Response.Status.NOT_FOUND);
+	}
+	return result.get();
     }
 
     @Override
     public MovieIdList findMovie(int year, String orderingField) {
-	// TODO Auto-generated method stub
-	return null;
+	if (!orderingField.toUpperCase().equals("DIRECTOR")
+		&& !orderingField.toUpperCase().equals("TITLE")) {
+	    throw new WebApplicationException(Response.Status.BAD_REQUEST);
+	}
+	List<MyMovie> searchedMovies = movies.stream()
+		.filter(movie -> movie.getMovie().getYear() == year)
+		.collect(Collectors.toList());
+	Collections.sort(searchedMovies, (a, b) -> {
+	    if (orderingField.toUpperCase().equals("DIRECTOR")) {
+		return a.getMovie().getDirector()
+			.compareTo(b.getMovie().getDirector());
+	    }
+	    return a.getMovie().getTitle().compareTo(b.getMovie().getTitle());
+	});
+	List<Integer> searchedIds = searchedMovies.stream()
+		.map(movie -> movie.getId()).collect(Collectors.toList());
+	return MovieIdList.newBuilder().addAllId(searchedIds).build();
     }
 
     @Override
     public MovieId createMovie(Movie movie) {
-	// TODO Auto-generated method stub
-	return null;
+	boolean isIdTaken = movies.stream().anyMatch(m -> m.getId() == nextId);
+	while (isIdTaken) {
+	    ++nextId;
+	    isIdTaken = movies.stream().anyMatch(m -> m.getId() == nextId);
+	}
+	MyMovie myMovie = new MyMovie();
+	myMovie.setId(nextId);
+	myMovie.setMovie(movie);
+	movies.add(myMovie);
+	++nextId;
+	return MovieId.newBuilder().setId(myMovie.getId()).build();
     }
 
     @Override
     public void upsertMovie(int id, Movie movie) {
-	// TODO Auto-generated method stub
+	if (id < 0) {
+	    throw new WebApplicationException(Response.Status.BAD_REQUEST);
+	}
+	Optional<MyMovie> result = movies.stream().filter(m -> m.getId() == id)
+		.collect(Collectors.reducing((a, b) -> null));
+	MyMovie myMovie = new MyMovie();
+	myMovie.setId(id);
+	myMovie.setMovie(movie);
+	if (result.isEmpty()) {
+	    movies.add(myMovie);
+	} else {
+	    int index = movies.indexOf(result.get());
+	    movies.set(index, myMovie);
+	}
     }
 
     @Override
     public void deleteMovie(int id) {
-	// TODO Auto-generated method stub
+	Optional<MyMovie> movieToDelete = movies.stream()
+		.filter(m -> m.getId() == id)
+		.collect(Collectors.reducing((a, b) -> null));
+	if (movieToDelete.isPresent()) {
+	    movies.remove(movieToDelete.get());
+	}
     }
-
 }
---
+++ src/main/java/movies/MyMovie.java
@@ -0,0 +1,24 @@
+package movies;
+
+import movies.Movies.Movie;
+
+public class MyMovie {
+    private int id;
+    private Movie movie;
+
+    public int getId() {
+	return id;
+    }
+
+    public void setId(int id) {
+	this.id = id;
+    }
+
+    public Movie getMovie() {
+	return movie;
+    }
+
+    public void setMovie(Movie movie) {
+	this.movie = movie;
+    }
+}
