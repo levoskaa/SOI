a3b7184b55eeaf807fbccca49c94b1c47837b4ec
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-07 10:52:39 +0100

    Implement GetMovies endpoint

--- app/services/movies_service.js
+++ app/services/movies_service.js
@@ -1,8 +1,17 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const movies_1 = require("../schemas/movies");
 const router = express.Router();
 router.get('/', (req, res) => {
+    movies_1.Movie.find({}, (err, movies) => {
+        if (err) {
+            res.json({ info: 'Error executing query.', error: err });
+        }
+        else {
+            res.json({ movie: movies });
+        }
+    });
 });
 exports.default = router;
 //# sourceMappingURL=movies_service.js.map
--- src/services/movies_service.ts
+++ src/services/movies_service.ts
@@ -2,13 +2,21 @@
 
 // Import express:
 import * as express from 'express';
+import { Movie } from '../schemas/movies';
 
 // Create a new express router:
 const router = express.Router();
 
-// 
+// Get movies
 router.get('/', (req, res) => {
     // TODO
+    Movie.find({}, (err, movies) => {
+        if (err) {
+            res.json({ info: 'Error executing query.', error: err });
+        } else {
+            res.json({ movie: movies });
+        }
+    })
 });
 
 // Export the router:
