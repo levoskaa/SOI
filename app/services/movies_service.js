2e7d96b7139deeacd143274cd7fc86062f8ac3e1
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-03-07 16:02:33 +0100

    Implement CreateMovie endpoint

--- app/services/movies_service.js
+++ app/services/movies_service.js
@@ -3,6 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const movies_1 = require("../schemas/movies");
 const router = express.Router();
+let nextId = 0;
+router.post('/', (req, res) => {
+    const movieToCreate = Object.assign({ _id: nextId++ }, req.body);
+    movies_1.Movie.create(movieToCreate, (err, movie) => {
+        if (err) {
+            res.json({ info: 'Error executing query.', error: err });
+        }
+        else {
+            res.json({ id: movie.id });
+        }
+    });
+});
 router.get('/', (req, res) => {
     movies_1.Movie.find({}, (err, movies) => {
         if (err) {
--- src/services/movies_service.ts
+++ src/services/movies_service.ts
@@ -2,10 +2,27 @@
 
 // Import express:
 import * as express from 'express';
-import { Movie } from '../schemas/movies';
+import { Movie, MovieEntity } from '../schemas/movies';
 
 // Create a new express router:
 const router = express.Router();
+
+let nextId = 0;
+
+// Create movie
+router.post('/', (req, res) => {
+    const movieToCreate = {
+        _id: nextId++,
+        ...req.body
+    }
+    Movie.create(movieToCreate, (err, movie) => {
+        if (err) {
+            res.json({ info: 'Error executing query.', error: err });
+        } else {
+            res.json({ id: movie.id });
+        }
+    })
+});
 
 // Get movies
 router.get('/', (req, res) => {
